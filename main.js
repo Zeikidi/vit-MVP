b6b12c3f8dbbbc4bbb5cfae9ce9e61db192f7101
zeiki <zeikidi@gmail.com>
2020-08-19 20:10:31 -0700

    updated changes to final MVP

--- main.js
+++ main.js
@@ -16,6 +16,18 @@ function myFunction() {
   }
 }
 
+/* Set the width of the side navigation to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
+
 dateHax();
 
 function dateHax() {
